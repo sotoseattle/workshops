26e2acabb9c480b2888d7e22a88e93ec2803fa6e
Ryan Davis <ryand-ruby@zenspider.com>
2015-02-18 21:25:09 -0800

    My first entry in GoL. The OO version. Happens to have no conditionals.

--- submissions/zenspider/object_oriented/lib/game_of_life.rb
+++ submissions/zenspider/object_oriented/lib/game_of_life.rb
@@ -1,3 +1,33 @@
 class GameOfLife
   VERSION = "1.0.0"
+
+  attr_accessor :cells
+
+  def initialize
+    self.cells = []
+  end
+
+  def update
+    cells.replace considered.select { |c| alive? c }.sort
+  end
+
+  def considered
+    cells.map { |c| neighbors_for(*c) }.flatten(1).uniq
+  end
+
+  MIN = { true => 2, false => 3 }
+
+  def alive? cell
+    count = (neighbors_for(*cell) & cells).size
+    min   = MIN[cells.include? cell]
+    count.between? min, 3
+  end
+
+  delta  = [-1, 0, 1]
+  same   = [0,0]
+  DELTAS = (delta.product(delta) - [same])
+
+  def neighbors_for x,y
+    DELTAS.map { |(dx,dy)| [x+dx, y+dy] }.reject { |(m,n)| m < 0 || n < 0 }
+  end
 end
--- submissions/zenspider/object_oriented/test/test_game_of_life.rb
+++ submissions/zenspider/object_oriented/test/test_game_of_life.rb
@@ -2,4 +2,67 @@ require "minitest/autorun"
 require "game_of_life"
 
 class TestGameOfLife < Minitest::Test
+  attr_accessor :gol
+
+  def setup
+    self.gol = GameOfLife.new
+  end
+
+  def test_facts
+    # there are cells
+    assert_equal [], gol.cells
+
+    gol.cells = []
+
+    # there is some sort of update method
+    assert gol.update
+
+    # cells are either alive or dead/non-existent
+    # on update, the rules are applied and the next iteration is calculated
+
+    # # neighbors | state
+    # <2          | dead
+    # >3          | dead
+    # 2..3        | stay live
+    # 3           | spawn new
+
+    assert_update [], []
+    assert_update [], [[1,1]]
+    assert_update [], [[1,1],[2,1]]
+    assert_update [], [[2,1],[1,1]]
+
+    # http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Examples_of_patterns
+    # block - stable 3
+    assert_update [[1,1],[2,1],[1,2],[2,2]], [[1,1],[2,1],[1,2],[2,2]]
+
+    # blinker - <2, 2, 3-spawn
+    assert_update [[0,2],[1,2],[2,2]], [[1,1],[1,2],[1,3]]
+
+    # toad - ... >3
+    assert_update([[1, 1], [1, 2], [2, 3], [3, 0], [4, 1], [4, 2]],
+                  [[2,1],[3,1],[4,1], [1,2],[2,2],[3,2]])
+    assert_update([[2,1],[3,1],[4,1], [1,2],[2,2],[3,2]],
+                  [[1, 1], [1, 2], [2, 3], [3, 0], [4, 1], [4, 2]])
+  end
+
+  def assert_update expect, input
+    gol.cells = input
+    assert gol.update
+    assert_equal expect.sort, gol.cells
+  end
+
+  def test_neighbors_for
+    exp = [[0,0], [1,0], [2,0],
+           [0,1],        [2,1],
+           [0,2], [1,2], [2,2]].sort
+
+    assert_equal exp, gol.neighbors_for(1,1)
+
+    exp = exp.map { |(x,y)| [x+1, y+1] }
+    assert_equal exp, gol.neighbors_for(2,2)
+
+    exp = [       [1,0],
+           [0,1], [1,1]].sort
+    assert_equal exp, gol.neighbors_for(0,0)
+  end
 end
